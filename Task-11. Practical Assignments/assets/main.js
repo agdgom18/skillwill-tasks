9462373fdd66f517eaea8429ba92784faf05a7f9
agdgom18 <agdgomelashvili18@gmail.com>
2023-06-07 11:05:07 +0400

    task-11 is done

--- Task-11. Practical Assignments/assets/main.js
+++ Task-11. Practical Assignments/assets/main.js
@@ -1,20 +1,42 @@
 // Task-1;
-function expo(a, b, c) {
-  if (b === 0) return 1;
-  return a * expo(a, b - 1);
-}
+// function expo(a, b, c) {
+//   if (b === 0) return 1;
+//   return a * expo(a, b - 1);
+// }
 
-// Task-2
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//   .then((res) => res.json())
-//   .then((res) => console.log(res));
-
+// Task - 2;
 async function getFetch() {
   try {
-    const rawData = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await rawData.json();
-    console.log(data);
-  } catch (error) {}
+    const data = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+    return data;
+  } catch (error) {
+    return console.error(error);
+  }
 }
-
-getFetch();
+//  Task-3
+function deepCopy(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+  let copy = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      copy[key] = deepCopy(obj[key]);
+    }
+  }
+  return copy;
+}
+async function asyncFnDeepCopy(obj) {
+  return new Promise((resolve, reject) => {
+    if (typeof obj !== 'object') {
+      reject('ERRRRRROOOOR');
+    }
+    const copy = deepCopy(obj);
+    resolve(copy);
+  });
+}
+async function fn() {
+  const data = await asyncFnDeepCopy(await getFetch());
+  console.log(data);
+}
+fn();
